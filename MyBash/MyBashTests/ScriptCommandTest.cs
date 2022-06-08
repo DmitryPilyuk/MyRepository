5affe3ff779ef63b8397a3376b07283d1fe6e6f8
DmitryPilyuk <st095257@student.spbu.ru>
2022-06-08 03:11:30 +0300

    Test changed

--- MyBash/MyBashTests/ScriptCommandTest.cs
+++ MyBash/MyBashTests/ScriptCommandTest.cs
@@ -11,7 +11,7 @@ namespace MyBashTests
 		[TestMethod]
 		public void TestWithCorrectArgs()
 		{
-			string expected = "Script started\nC:\\2term\\MyRepository\\MyBash\\MyBashTests\\bin\\Debug\\net6.0\\testFiles\nscript.txt\ntestFile1.txt\ntestFile2.txt\ntestWrite.txt\nThis text from testFile2\n2 13 62 testFile1.txt\n1 4 24 testFile2.txt\n3 17 86 итого\n";
+			string expected = "Script started\nC:\\2term\\MyRepository\\MyBash\\MyBashTest\\bin\\Release\\net6.0\\testFiles\nscript.txt\ntestFile1.txt\ntestFile2.txt\ntestWrite.txt\nThis text from testFile2\n2 13 62 testFile1.txt\n1 4 24 testFile2.txt\n3 17 86 итого\n";
 			MyBash.MyBash bash = new MyBash.MyBash();
 			bash.Path =  $"{Directory.GetCurrentDirectory()}\\testFiles";
 			ScriptCommand script = new ScriptCommand(bash, $"{Directory.GetCurrentDirectory()}\\testFiles\\script.txt", (_) => true);
